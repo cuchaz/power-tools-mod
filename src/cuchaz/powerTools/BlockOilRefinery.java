249022d2118416fc38eed2203657dad0f2d39261
jeff <devnull@localhost>
2013-05-21 01:41:11 -0400

    gave the refinery an inventory and added power detection with wheel animation indicator

--- src/cuchaz/powerTools/BlockOilRefinery.java
+++ src/cuchaz/powerTools/BlockOilRefinery.java
@@ -1,9 +1,13 @@
 package cuchaz.powerTools;
 
+import java.util.Random;
+
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Icon;
@@ -14,18 +18,23 @@ import cpw.mods.fml.relauncher.SideOnly;
 
 public class BlockOilRefinery extends BlockContainer
 {
-	// based on BlockFurnace
-	
 	@SideOnly( Side.CLIENT )
 	private Icon m_iconFront;
 	@SideOnly( Side.CLIENT )
-	private Icon m_iconSide;
+	private Icon[] m_iconSide;
 	@SideOnly( Side.CLIENT )
 	private Icon m_iconTop;
 	
 	protected BlockOilRefinery( int blockId )
 	{
 		super( blockId, Material.iron );
+		
+		setHardness( 5.0F );
+		setResistance( 10.0F );
+		setStepSound( soundMetalFootstep );
+		setUnlocalizedName( "blockOilRefinery" );
+		
+		m_iconSide = new Icon[] { null, null };
 	}
 	
 	@Override
@@ -53,12 +62,13 @@ public class BlockOilRefinery extends BlockContainer
 			targetSide = targetSide.getXZNextSide();
 		}
 		
+		int wheelFrame = getMetaWheelFrame( meta );
 		switch( targetSide )
 		{
 			case North: return m_iconFront;
-			case West: return m_iconSide;
+			case West: return m_iconSide[wheelFrame];
 			case South: return blockIcon;
-			case East: return m_iconSide;
+			case East: return m_iconSide[wheelFrame];
 		}
 		
 		// if all else fails
@@ -72,7 +82,8 @@ public class BlockOilRefinery extends BlockContainer
 		// UNDONE: can change textures here based on state
 		blockIcon = iconRegister.registerIcon( "powerTools:oilRefineryBack" );
 		m_iconFront = iconRegister.registerIcon( "powerTools:oilRefineryFront" );
-		m_iconSide = iconRegister.registerIcon( "powerTools:oilRefinerySide" );
+		m_iconSide[0] = iconRegister.registerIcon( "powerTools:oilRefinerySide1" );
+		m_iconSide[1] = iconRegister.registerIcon( "powerTools:oilRefinerySide2" );
 		m_iconTop = iconRegister.registerIcon( "powerTools:oilRefineryTop" );
 	}
 	
@@ -101,15 +112,91 @@ public class BlockOilRefinery extends BlockContainer
 			break;
 		}
 		
-		// calculate the ccw north-front offset
-		int offset = 0;
+		// calculate the ccw north-front rotation
+		int rotation = 0;
 		while( side != BlockSide.North )
 		{
 			side = side.getXZNextSide();
-			offset++;
+			rotation++;
 		}
 		
 		// set the offset as the block meta
-		world.setBlockMetadataWithNotify( x, y, z, offset, FlagSendChangeToClients );
+		world.setBlockMetadataWithNotify( x, y, z, computeMeta( rotation, 0 ), FlagSendChangeToClients );
     }
+	
+	@Override
+	public boolean onBlockActivated( World world, int x, int y, int z, EntityPlayer player, int par6, float par7, float par8, float par9 )
+	{
+		// ignore for clients (taken from BlockFurnace... but this seems backwards?)
+		if( world.isRemote )
+		{
+			return true;
+		}
+		
+		TileEntityOilRefinery tileEntity = (TileEntityOilRefinery)world.getBlockTileEntity( x, y, z );
+		if( tileEntity != null )
+		{
+			player.displayGUIChest( tileEntity.getInventory() );
+		}
+		
+		return true;
+	}
+	
+	@Override
+	public void breakBlock( World world, int x, int y, int z, int side, int meta )
+	{
+		Random rand = new Random();
+		
+		// eject the items from the refinery
+		TileEntityOilRefinery tileEntity = (TileEntityOilRefinery)world.getBlockTileEntity( x, y, z );
+		for( int i=0; i<tileEntity.getInventory().getSizeInventory(); i++ )
+		{
+			// get the item in slot i
+			ItemStack itemStack = tileEntity.getInventory().getStackInSlot( i );
+			if( itemStack == null )
+			{
+				continue;
+			}
+			
+			// pick a random spot for the item in this block
+			double dx = rand.nextDouble() * 0.8 + 0.1;
+			double dy = rand.nextDouble() * 0.8 + 0.1;
+			double dz = rand.nextDouble() * 0.8 + 0.1;
+			
+			// pick a random direction for the item
+			double variance = 0.05;
+			double extraUpSpeed = 0.2f;
+			double vx = rand.nextGaussian() * variance;
+			double vy = rand.nextGaussian() * variance + extraUpSpeed;
+			double vz = rand.nextGaussian() * variance;
+			
+			// eject the item into the world
+			EntityItem entityItem = new EntityItem(
+				world,
+				dx + x, dy + y, dz + z,
+				new ItemStack( itemStack.itemID, itemStack.stackSize, itemStack.getItemDamage() )
+			);
+			entityItem.motionX = vx;
+			entityItem.motionY = vy;
+			entityItem.motionZ = vz;
+			world.spawnEntityInWorld( entityItem );
+		}
+		
+		super.breakBlock( world, x, y, z, side, meta );
+	}
+	
+	public static int getMetaRotation( int meta )
+	{
+		return meta & 0x3;
+	}
+	
+	public static int getMetaWheelFrame( int meta )
+	{
+		return ( meta & 0x4 ) >> 2;
+	}
+	
+	public static int computeMeta( int rotation, int wheelFrame )
+	{
+		return ( ( wheelFrame & 0x1 ) << 2 ) | ( rotation & 0x3 );
+	}
 }
--- src/cuchaz/powerTools/TileEntityOilRefinery.java
+++ src/cuchaz/powerTools/TileEntityOilRefinery.java
@@ -1,8 +1,127 @@
 package cuchaz.powerTools;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import net.minecraft.block.Block;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.InventoryBasic;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ChunkCoordinates;
 
 public class TileEntityOilRefinery extends TileEntity
 {
-	// based on TileEntityFurance
+	private static final String InventoryName = "Oil Refinery";
+	private static final int InventorySize = 9; // needs to be a multiple of 9 or the GUI won't work
+	
+	private InventoryBasic m_inventory;
+	private int m_delayCounter;
+	
+	public TileEntityOilRefinery( )
+	{
+		m_inventory = new InventoryBasic( InventoryName, false, InventorySize );
+		m_delayCounter = 0;
+	}
+	
+	public IInventory getInventory( )
+	{
+		return m_inventory;
+	}
+	
+	public boolean isUseableByPlayer( EntityPlayer player )
+	{
+		if( worldObj.getBlockTileEntity( xCoord, yCoord, zCoord ) != this )
+		{
+			return false;
+		}
+
+		return player.getDistanceSq( xCoord + 0.5, yCoord + 0.5, zCoord + 0.5 ) <= 64;
+	}
+	
+	// UNDONE: implement save state stuff
+	
+	private boolean isDelayedUpdate( )
+	{
+		boolean isDelayedUpdate = m_delayCounter == 0;
+		m_delayCounter = ( m_delayCounter + 1 ) % 6;
+		return isDelayedUpdate;
+	}
+	
+	@Override
+	public void updateEntity( )
+	{
+		if( !isDelayedUpdate() )
+		{
+			return;
+		}
+		
+		// UNDONE: handle coal processing
+		
+		// TEMP
+		System.out.println( "Update: " + ( isPowered() ? "Powered!" : "Not powered... =(" ) );
+		
+		// get the wheel frame
+		int meta = worldObj.getBlockMetadata( xCoord, yCoord, zCoord );
+		int oldWheelFrame = BlockOilRefinery.getMetaWheelFrame( meta );
+		
+		// update the wheel frame if needed
+		int newWheelFrame = 0;
+		if( isPowered() )
+		{
+			// spin the wheels
+			newWheelFrame = oldWheelFrame == 0 ? 1 : 0;
+		}
+		
+		// update the metadata if needed
+		if( newWheelFrame != oldWheelFrame )
+		{
+			// TEMP
+			System.out.println( "Setting wheel frame to: " + newWheelFrame + " (" + BlockOilRefinery.computeMeta( BlockOilRefinery.getMetaRotation( meta ), newWheelFrame ) + ")" );
+			
+			final int FlagSendChangeToClients = 2;
+			worldObj.setBlockMetadataWithNotify(
+				xCoord, yCoord, zCoord,
+				BlockOilRefinery.computeMeta( BlockOilRefinery.getMetaRotation( meta ), newWheelFrame ),
+				FlagSendChangeToClients
+			);
+		}
+	}
+	
+	public boolean isPowered( )
+	{
+		// along which axis are the sides?
+		boolean sidesAreNorthSouth = BlockOilRefinery.getMetaRotation( worldObj.getBlockMetadata( xCoord, yCoord, zCoord ) ) % 2 == 0;
+		
+		// get a list of the blocks on the sides
+		List<ChunkCoordinates> blocks = new ArrayList<ChunkCoordinates>();
+		if( sidesAreNorthSouth )
+		{
+			blocks.add( new ChunkCoordinates( xCoord, yCoord, zCoord + 1 ) );
+			blocks.add( new ChunkCoordinates( xCoord, yCoord, zCoord - 1 ) );
+		}
+		else
+		{
+			blocks.add( new ChunkCoordinates( xCoord + 1, yCoord, zCoord ) );
+			blocks.add( new ChunkCoordinates( xCoord - 1, yCoord, zCoord ) );
+		}
+		
+		// does either side have a flowing water block?
+		int targetId = Block.waterStill.blockID;
+		for( ChunkCoordinates coords : blocks )
+		{
+			if( worldObj.getBlockId( coords.posX, coords.posY, coords.posZ ) != targetId )
+			{
+				continue;
+			}
+			
+			// is the block flowing? (meta 1-8 indicates flowing water)
+			if( worldObj.getBlockMetadata( coords.posX, coords.posY, coords.posZ ) > 0 )
+			{
+				return true;
+			}
+		}
+		
+		return false;
+	}
 }
